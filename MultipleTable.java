9cf46a051a924dabb8472d50b5c6eb2b791b95f2
Serkan HELVACIOGLU <serkanhelvacioglu@outlook.com>
2017-10-10 16:34:48 +0300

    MultipleTable
    Multiple Table from 1 to 10.

---
+++ MultipleTable.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+	public class Multiply{
+		
+		
+		public static void main (String[] args){
+			
+			int firstNum, secondNum;
+			
+			for(firstNum=1;firstNum<=10;firstNum++){
+				
+				for(secondNum=1;secondNum<=10;secondNum++){
+					
+					System.out.println(firstNum+"x"+secondNum+"="+(firstNum*secondNum));
+					
+				}
+				System.out.println();
+			}
+			
+		}
+	}
+	
