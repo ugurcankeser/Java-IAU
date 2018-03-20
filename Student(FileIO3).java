c0e52f48e1cca074d717bcb2f67a8108699603d2
Uğurcan Keser <32204093+ugurcankeser@users.noreply.github.com>
2018-03-20 13:25:45 +0300

    Rename Student.java to Student(FileIO3).java

---
+++ Student(FileIO3).java
@@ -0,0 +1,31 @@
+package Unit;
+
+public class Student {
+	String name,surname,departmant;
+	int number;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getDepartmant() {
+		return departmant;
+	}
+	public void setDepartmant(String departmant) {
+		this.departmant = departmant;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+}
