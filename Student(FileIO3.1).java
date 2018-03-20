83f18949fb010af1afcfa3f7a7e56c52b0e965ec
Uğurcan Keser <32204093+ugurcankeser@users.noreply.github.com>
2018-03-20 13:33:39 +0300

    Create Student(FileIO3.1).java

---
+++ Student(FileIO3.1).java
@@ -0,0 +1,34 @@
+package pack1;
+
+public class Student {
+
+	String name, surname, department;
+	int number;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+	
+}
